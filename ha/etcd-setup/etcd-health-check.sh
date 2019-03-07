f24a82968365fd5a49acb464d483fafe6ac92fa2
lc-kubeadm <46482212+lc-kubeadm@users.noreply.github.com>
2019-03-07 10:17:55 +0530

    Delete etcd-health-check.sh

--- ha/etcd-setup/etcd-health-check.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm -it \
---net host \
--v /etc/kubernetes:/etc/kubernetes quay.io/coreos/etcd:${ETCD_TAG} etcdctl \
---cert-file /etc/kubernetes/pki/etcd/peer.crt \
---key-file /etc/kubernetes/pki/etcd/peer.key \
---ca-file /etc/kubernetes/pki/etcd/ca.crt \
---endpoints https://${HOST0}:2379 cluster-health
