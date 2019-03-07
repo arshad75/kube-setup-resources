019278b5922b61f2355c2d7bb97bba673eedff67
lc-kubeadm <46482212+lc-kubeadm@users.noreply.github.com>
2019-03-07 14:38:49 +0530

    updated setup scripts -

--- ha/main-master.sh
+++ ha/main-master.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+KUBELET_VERSION="1.13.2-00"
+KUBEADM_VERSION="1.13.2-00"
+KUBECTL_VERSION="1.13.2-00"
+DOCKER_VERSION="18.06.1~ce~3-0~ubuntu"
+
+
+echo " ------------------ Kubeadm kubernetes installer ----------------"
+echo " ------------------ installer for Worker node    ----------------"
+
+apt-mark unhold kubelet kubeadm docker-ce
+
+#apt purge kubelet kubeadm kubectl docker-ce
+
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -13,7 +26,7 @@ apt-get install \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common -y 
+    software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -26,10 +39,10 @@ add-apt-repository \
 
 
 apt-get update
+apt-get install  kubelet=$KUBELET_VERSION kubeadm=$KUBEADM_VERSION docker-ce=$DOCKER_VERSION
+apt install kubectl=$KUBECTL_VERSION && apt-mark hold kubectl
 
-apt-get install -y kubelet kubeadm kubectl docker-ce=18.06.1~ce~3-0~ubuntu
-apt-mark hold kubelet kubeadm kubectl docker-ce
-
+apt-mark hold kubelet kubeadm  docker-ce
 echo " ------------------ Kubeadm kubernetes installer | Finised  ----------------"
 
 echo " ------------------ Initiating Kubernetes Cluster Main Master  ----------------"
@@ -51,7 +64,7 @@ kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl versio
 
 echo " ------------------ Installing the Dashboard ----------------"
 
-kubectl apply -f https://gist.githubusercontent.com/lc-kubeadm/5b8896b47ee046d33d73d51ef9dabc48/raw/d89fe9c5446dd9ee4b412d219c308bf2205003e0/kubernetes-dashboard.yaml
+kubectl apply -f kubernetes-dashboard.yaml
 
 echo " ------------------ Extracting the Dashboard Login Certificates ----------------"
 
